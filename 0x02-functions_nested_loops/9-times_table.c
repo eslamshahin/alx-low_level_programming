0b83f07aec202e79479b381c77d083f7add1a5dc
eslamshahin <eslamshahin01229616226@gmail.com>
2022-12-14 07:01:58 -0800

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,11 +6,11 @@
 void times_table(void)
 {
 	int num, mult, prod;
-
-for (num = 0; num <= 9; num++)
+	
+	for (num = 0; num <= 9; num++)
 {
 	_putchar('0');
-	
+
 	for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
@@ -26,5 +26,5 @@ for (num = 0; num <= 9; num++)
 			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
-	}
+}
 }
