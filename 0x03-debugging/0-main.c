82400ed99ff1259118b1d0b78e4c0b96492f69c7
eslamshahin <eslamshahin01229616226@gmail.com>
2022-12-14 09:15:35 -0800

    0 Task

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - test function that print if integer is positive or negative
+ * main - test functioin for positive or negative
  * Return:0
  */
 
@@ -10,7 +10,7 @@ int main(void)
 int i;
 
 i = 0;
-positive_or_negative(1);
+positive_or_negative(i);
 
 return (0);
 }
