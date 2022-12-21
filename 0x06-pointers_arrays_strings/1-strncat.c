3a357453134443af0f4dc5a9e4eb8402128fe50e
eslamshahin <eslamshahin01229616226@gmail.com>
2022-12-21 08:15:19 -0800

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strcat - concatinate 2 strings.
+ * @dest: first string.
+ * @src: second string.
+ * Return: string.
+ */
+char *_strcat(char *dest, char *src)
+{
+int i = 0, k = 0;
+
+while (dest[i] != '\0')
+i++;
+
+while (src[k] != '\0' && n > k)
+{
+dest[i] = src[k];
+k++;
+i++;
+}
+if (n > 0)
+{
+dest[i] = '\0';
+}
+
+return (dest);
+}
