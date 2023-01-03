4eac93e194012707b2077d7bc0976257e82e7e4d
eslamshahin <eslamshahin01229616226@gmail.com>
2023-01-03 07:00:56 -0800

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,14 @@
 #include "main.h"
+
 /**
  * _memcpy - function that copies memory area
- * @dest: the address of memory to print  *@src: second value
+ * @dest: the address of memory to print
+ * @src: second value
  * @n: thirth value
  * Return: char with result of memset
  */
-char *_memcpy(char *dest, char *src, unsigned int n)
+
+char *_memcpy(char *dest, char *src, unsigned int n);
 {
 	unsigned int i;
 
