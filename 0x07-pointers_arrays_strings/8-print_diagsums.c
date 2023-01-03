6f9b929ae6d6438afa899888e9711336696665e3
eslamshahin <eslamshahin01229616226@gmail.com>
2023-01-03 07:13:56 -0800

    task 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,19 +1,26 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * print_chessboard - function that prints the chessboard.
+ * print_diagsums - function that prints the sum of the two diagonals
  * @a: first value -char
- *
+ * @size: second value -int
  */
-void print_chessboard(char (*a)[8])
+void print_diagsums(int *a, int size)
 {
-	int i, j;
+	int i, j, suma_1 = 0, suma_2 = 0, k, l = 0;
 
-	for (i = 0; i < 8; i++)
+	k = size - 1;
+	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < 8; j++)
+		for (j = 0; j < size; j++)
 		{
-			_putchar (a[i][j]);
+			if (i == j)
+				suma_1 += a[l];
+			if (j == k)
+				suma_2 += a[l];
+			l++;
 		}
-		_putchar('\n');
+		k--;
 	}
+	printf("%i, %i\n", suma_1, suma_2);
 }
